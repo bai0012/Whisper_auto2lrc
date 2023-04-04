dd34dfd31b1e1f2088a75930aafe6c1fc776ffaf
bai0012 <53823526+bai0012@users.noreply.github.com>
2023-04-04 13:10:57 +0800

    Add files via upload

--- srt_to_lrc.py
+++ srt_to_lrc.py
@@ -1,37 +1,29 @@
-import os
-import re
 import sys
+import os
 
-def srt_to_lrc(srt_path):
-    # 读取srt文件
-    with open(srt_path, 'r', encoding='utf-8') as f:
-        srt_content = f.read()
+def srt_to_lrc(srt_file_path):
+    lrc_file_path = srt_file_path.replace('.srt', '.lrc')
+    with open(srt_file_path, 'r', encoding='utf-8-sig') as srt_file, \
+         open(lrc_file_path, 'w', encoding='utf-8-sig') as lrc_file:
+        lines = srt_file.readlines()
+        for i in range(0, len(lines), 4):
+            time_start, time_end = lines[i+1].strip().split(' --> ')
+            time_start = convert_time(time_start)
+            time_end = convert_time(time_end)
+            lrc_file.write(f"[{time_start}]{lines[i+2]}")
+            lrc_file.write(f"[{time_end}]\n")
 
-    # 使用正则表达式提取出每行字幕的时间轴和内容
-    pattern = r'(\d+)\n(\d{2}:\d{2}:\d{2},\d{3}) --> (\d{2}:\d{2}:\d{2},\d{3})\n(.*?)\n\n'
-    matches = re.findall(pattern, srt_content, re.DOTALL)
+    os.remove(srt_file_path)
+    #print(f'Successfully converted {srt_file_path} to {lrc_file_path}')
 
-    # 将时间轴转换为lrc格式
-    lrc_content = ''
-    for i, match in enumerate(matches):
-        start_time = match[1].replace(',', '.')
-        lrc_content += f'[{start_time}] {match[3]}'
-
-        # 如果不是最后一行字幕，则需要添加换行符
-        if i != len(matches) - 1:
-            lrc_content += '\n'
-
-    # 将lrc内容写入同名文件，但后缀名为.lrc
-    lrc_path = os.path.splitext(srt_path)[0] + '.lrc'
-    with open(lrc_path, 'w', encoding='utf-8') as f:
-        f.write(lrc_content)
-
-    # 删除原srt文件
-    os.remove(srt_path)
+def convert_time(time_string):
+    time_parts = time_string.split(':')
+    minutes = int(time_parts[0]) * 60 + int(time_parts[1])
+    seconds = float(time_parts[2].replace(',', '.'))
+    return f"{minutes:02d}:{seconds:05.2f}"
 
 if __name__ == '__main__':
-    # 从命令行参数中获取srt文件路径
-    srt_path = sys.argv[1]
-
-    # 调用srt_to_lrc函数进行转换和删除操作
-    srt_to_lrc(srt_path)
+    if len(sys.argv) != 2:
+        print('Wrong!')
+    else:
+        srt_to_lrc(sys.argv[1])
--- whisper.ps1
+++ whisper.ps1
@@ -1,8 +1,30 @@
-# 设置要转换的文件夹路径
-$folderPath = "C:\Path\To\Audio\Folder"
+# 请求用户输入要转换的文件夹路径
+$folderPath = Read-Host "Please enter the folder path to convert audio files"
+
+# 请求用户选择 --model 参数的值
+Write-Host "Please enter the model you want to use"
+Write-Host "1. tiny"
+Write-Host "2. base"
+Write-Host "3. small"
+Write-Host "4. medium"
+Write-Host "5. large"
+$modelChoice = Read-Host "default is small"
+
+# 根据用户选择设置 --model 参数的值
+switch ($modelChoice) {
+    "1" { $model = "tiny" }
+    "2" { $model = "base" }
+    "3" { $model = "small" }
+    "4" { $model = "medium" }
+    "5" { $model = "large" }
+    default { $model = "small" }
+}
+
+# 请求用户输入 --language 参数的值
+$language = Read-Host "Please enter the audio's language"
 
 # 获取要转换的所有音频文件
-$audioFiles = Get-ChildItem $folderPath -Recurse -Include "*.mp3", "*.wav", "*.flac", "*.m4a", "*.aac", "*.wma", "*.ogg"
+$audioFiles = Get-ChildItem $folderPath -Recurse -Include "*.mp3", "*.wav"
 
 # 设置进度条初始值
 $i = 0
@@ -12,27 +34,26 @@ foreach ($file in $audioFiles) {
     # 更新进度条
     Write-Progress -Activity "Converting audio files..." -Status "Processing file $($file.Name) ($($i+1) of $($audioFiles.Count))" -PercentComplete (($i+1)/$audioFiles.Count*100)
     
-    # 检查相应的.lrc文件是否已存在
-    $lrcFile = Get-ChildItem $file.DirectoryName -Include "*.lrc" -Recurse | Where-Object { $_.Name -match $file.BaseName }
-    if ($lrcFile) {
-        Write-Host "Skipping file $($file.Name) as .lrc file already exists."
+    # 获取同名的 .lrc 文件，如果存在则直接跳过转换
+    $lrcFile = $file.DirectoryName + "\" + $file.BaseName + ".lrc"
+    if (Test-Path $lrcFile) {
+        $i++
+        continue
     }
-    else {
-        # 执行Whisper命令将音频转换为文本
-        whisper --model small --language ja $file.FullName
-        
-        # 获取相应的.srt文件并调用Python程序将其转换为.lrc文件
-        $srtFile = Get-ChildItem $file.DirectoryName -Include "*.srt" -Recurse | Where-Object { $_.Name -match $file.BaseName }
-        if ($srtFile) {
-            $lrcFile = $srtFile.FullName.Replace(".srt", ".lrc")
-            & python srt_to_lrc.py $srtFile.FullName $lrcFile
-        }
-        
-        # 删除由Whisper生成的文件
-        $whisperFiles = Get-ChildItem $file.DirectoryName -Include "*.json", "*.tsv", "*.txt", "*.vtt" -Recurse | Where-Object { $_.Name -match $file.BaseName }
-        foreach ($whisperFile in $whisperFiles) {
-            Remove-Item $whisperFile.FullName -Force
-        }
+    
+    # 执行 Whisper 命令将音频转换为文本
+    whisper --model $model --language $language $file.FullName
+    
+    # 获取同名的 .srt 文件并调用 Python 程序将其转换为 .lrc 文件
+    $srtFile = $file.DirectoryName + "\" + $file.BaseName + ".srt"
+    if (Test-Path $srtFile) {
+        python srt_to_lrc.py "$srtFile"
+    }
+    
+    # 删除由 Whisper 生成的文件
+    $whisperFiles = Get-ChildItem $file.DirectoryName -Include "*.json", "*.tsv", "*.txt", "*.vtt" -Recurse | Where-Object { $_.Name -match $file.BaseName }
+    foreach ($whisperFile in $whisperFiles) {
+        Remove-Item $whisperFile.FullName -Force
     }
     
     # 增加进度条计数
@@ -41,3 +62,6 @@ foreach ($file in $audioFiles) {
 
 # 更新进度条，指示转换已完成
 Write-Progress -Activity "Converting audio files..." -Status "Conversion completed." -Completed
+
+# 显示 "Finish" 并黄色高亮显示
+Write-Host "Finish" -ForegroundColor Yellow
